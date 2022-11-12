1328a856066d9fe466548d3b1a922b5363b6986f
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2022-11-13 00:02:33 +0300

    almost done, remain a bit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,41 @@
 function main() {
     loadpage(transitFromLoadPage)
     typeTestPage.init(transitToTestSubtypes)
+    inDevTest.setEvents(onBack)
+    subtypesTestPage.setEvents()
+    testPage.setEvents()
 }
 
 function transitToTestSubtypes(type) {
     typeTestPage.clearStyles()
+
+    const typeTest = dataTests[type]
+
+    if(!typeTest.isComplete) {
+        onBack.prev = document.querySelector('.type-test')
+        onBack.current = inDevTest.getRoot()
+        transitToErrorPage(document.querySelector('.type-test'),typeTest.title)
+        return
+    }
+
     pageTransit(
         document.querySelector('.type-test'),
         document.querySelector('.subtype-test')
     )
 }
 
+function onBack() {
+    pageTransit(
+        onBack.current,
+        onBack.prev
+    )
+}
+
 function transitToTestTypes(slug) {
 }
 
-function transitToErrorPage() {
-
+function transitToErrorPage(prevDomEl, title) {
+    inDevTest.toThisPage(prevDomEl, title)
 }
 function transitToTests(slug) {
 
--- src/scripts/typeTestPage.js
+++ src/scripts/typeTestPage.js
@@ -30,7 +30,10 @@ const typeTestPage = (() => {
         underline.classList.add('type-test__type-underline_active')
     }
     const clearUnderlinesStyle = () => {
-
+        const nodeArray = document.querySelectorAll('.type-test__type-underline')
+        for(let node of nodeArray) {
+            node.classList.remove('type-test__type-underline_active')
+        }
     }
 
     const setEvent = () => {
@@ -53,5 +56,136 @@ const typeTestPage = (() => {
         render,
         destroy,
         clearStyles
+    }
+})()
+const inDevTest = (() => {
+    const domRoot = document.querySelector('.in-dev-test')
+
+    const backBtnTitle = document.querySelector('.in-dev-test__back')
+    const backBtn = document.querySelector('.in-dev-test__choose-tests')
+
+    const setEvents = (backOneCb) => {
+        backBtnTitle.addEventListener('click', backOneCb)
+        backBtn.addEventListener('click', goBack)
+    }
+
+    const toThisPage = (from, title) => {
+        backBtnTitle.querySelector('.in-dev-test__back-text').innerHTML = title
+
+        pageTransit(
+            from,
+            domRoot
+        )
+    }
+
+    const getRoot = () => {
+        return domRoot
+    }
+
+    const goBack = () => {
+        pageTransit(
+            domRoot,
+            document.querySelector('.type-test')
+        )
+    }
+
+    return {
+        setEvents,
+        toThisPage,
+        getRoot
+    }
+})()
+const testPage = (() => {
+    const domRoot = document.querySelector('.test-walkthrough')
+    const domCount = domRoot.querySelector('.test-walkthrough__count')
+    let tests = []
+
+    let rightCount = 0
+
+    const setEvents = () => {
+        domRoot.querySelector('.test-walkthrough__back').addEventListener('click', () => {
+            rightCount = 0
+
+            pageTransit(
+                domRoot,
+                subtypesTestPage.getDomRoot()
+            )
+        })
+
+        domRoot.querySelector('.test-walkthrough__back').addEventListener('click', prev)
+        domRoot.querySelector('.test-walkthrough__next').addEventListener('click', next)
+        domRoot.querySelector('.test-walkthrough__question-container').addEventListener('click', onAnswerClick)
+    }
+
+    const initTest = () => {
+
+    }
+
+    const onAnswerClick = (e) => {
+
+    }
+
+    const next = () => {
+
+    }
+    const prev = () => {
+
+    }
+
+    const toThisPage = (from, test) => {
+        tests = test
+
+        initTest()
+
+        pageTransit(
+            from,
+            domRoot
+        )
+    }
+
+    return {
+        setEvents,
+        toThisPage
+    }
+})()
+const subtypesTestPage = (() => {
+    const domRoot = document.querySelector('.subtype-test')
+
+    const setEvents = () => {
+        domRoot.querySelector('.subtype-test__back').addEventListener('click', () => {
+            pageTransit(
+                domRoot,
+                document.querySelector('.type-test')
+            )
+        })
+        domRoot.querySelector('.subtype-test__subtypes').addEventListener('click', e => {
+            goNextPage(e.target)
+        })
+
+    }
+
+    const getDomRoot = () => {
+        return domRoot
+    }
+
+    const goNextPage = (target) => {
+        const attr = target.getAttribute('data-test-subtype')
+        if(!tests[attr]) {
+            onBack.prev = domRoot
+            onBack.current = inDevTest.getRoot()
+            transitToErrorPage(
+                domRoot,
+                'психология'
+            )
+
+            return
+        }
+
+        testPage.toThisPage(domRoot, tests[attr])
+    }
+
+    return {
+        setEvents,
+        getDomRoot
     }
 })()
